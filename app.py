87cb58f2cd4e1b395dc4ecea886a2590ce70fd38
Ars-001-ps <sharonov.2021@inbox.ru>
2026-05-20 16:48:29 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,21 @@
-from flask import Flask
+from flask import Flask  # type: ignore
 from models import Employee, db
+from flask import render_template  # type: ignore
+
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///employees.db'
 db.init_app(app)
+all_employees = None
+
+
+@app.route('/employees')
+def employees():
+    all_employees = Employee.query.all()
+    return render_template('employees.html', employees=all_employees) 
+
+
+
 with app.app_context():
     db.create_all()
 if __name__ == '__main__':
-    app.run(debug = True)
+    app.run(debug=True)
