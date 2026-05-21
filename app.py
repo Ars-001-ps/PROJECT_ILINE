7bc6bed8ca83628b37001cbc67ec5fd5a408eb32
Ars-001-ps <sharonov.2021@inbox.ru>
2026-05-21 20:29:52 +0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
-from flask import Flask  # type: ignore
+from sqlalchemy import func
 from models import Employee, db
-from flask import render_template  # type: ignore
+from flask import render_template, request, Flask # type: ignore
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///employees.db'
@@ -10,8 +10,19 @@ all_employees = None
 
 @app.route('/employees')
 def employees():
+    search_query = request.args.get('search', '')
     all_employees = Employee.query.all()
-    return render_template('employees.html', employees=all_employees) 
+    
+    if search_query:
+        result = []
+        for emp in all_employees:
+            if search_query.lower() in emp.full_name.lower():
+                result.append(emp)
+        all_employees = result
+        
+    return render_template('employees.html', employees=all_employees)
+        
+    
 
 
 
@@ -19,3 +30,13 @@ with app.app_context():
     db.create_all()
 if __name__ == '__main__':
     app.run(debug=True)
+
+
+
+
+
+
+
+
+    
+     
